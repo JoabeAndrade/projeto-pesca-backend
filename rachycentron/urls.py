57e068716ead4d2c2d7052953500d2b8a022ffac
Joabe Andrade do Nascimento <joabeandrade2k01@gmail.com>
2025-08-14 12:39:28 -0300

    Corrige urls.py para não carregar debug_toolbar em produção

--- rachycentron/urls.py
+++ rachycentron/urls.py
@@ -3,22 +3,21 @@ URL configuration for rachycentron project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
-from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf import settings # <-- IMPORTANTE: Adicione esta linha
 
+# Suas URLs principais
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pescadores.urls')),
-] + debug_toolbar_urls()
+]
+
+# Este bloco só será executado se DEBUG for True (ou seja, no seu computador)
+if settings.DEBUG:
+    # A importação é feita aqui dentro para não dar erro em produção
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
